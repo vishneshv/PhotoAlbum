7852ea43debfd758e9340e37ae4f5ff5467e90c8
Vishnesh Vojjala <88830952+vishneshv@users.noreply.github.com>
2023-08-11 23:33:25 +0530

    Add files via upload

---
+++ src/components/body.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Body(){
+    return(
+        <div>
+        <div >
+                <h1 id="abc">Hover on me to see Clear Picture</h1>
+        </div>
+        <div id="gallery">
+        <img src="https://randomuser.me/api/portraits/men/1.jpg" id="images" alt="p1" />
+        <img src="https://randomuser.me/api/portraits/men/2.jpg" id="images" alt="p2"/>
+        <img src="https://randomuser.me/api/portraits/men/3.jpg" id="images" alt="p3" />
+        <img src="https://randomuser.me/api/portraits/men/4.jpg" id="images" alt="p4"/>
+        <img src="https://randomuser.me/api/portraits/men/5.jpg" id="images" alt="p5"/>
+        <img src="https://randomuser.me/api/portraits/men/6.jpg" id="images" alt="p6"/>
+        <img src="https://randomuser.me/api/portraits/men/7.jpg" id="images" alt="p7"/>
+        <img src="https://randomuser.me/api/portraits/men/8.jpg" id="images" alt="p8"/>
+        <img src="https://randomuser.me/api/portraits/men/9.jpg" id="images" alt="p9" />
+        <img src="https://randomuser.me/api/portraits/men/10.jpg" id="images" alt="p10"/>
+        <img src="https://randomuser.me/api/portraits/men/11.jpg" id="images" alt="p11" />
+        <img src="https://randomuser.me/api/portraits/men/12.jpg" id="images" alt="p12"/>
+        <img src="https://randomuser.me/api/portraits/men/13.jpg" id="images" alt="p13"/>
+        <img src="https://randomuser.me/api/portraits/men/14.jpg" id="images" alt="p14" />
+        <img src="https://randomuser.me/api/portraits/men/15.jpg" id="images" alt="p15"/>
+        <img src="https://randomuser.me/api/portraits/men/16.jpg" id="images" alt="p16"/>
+        </div>
+        </div>
+    )
+}
+
+export default Body;
---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer(){
+    return(
+        <div>
+            <p id="end">End Of Album</p>
+        </div>
+    )
+}
+export default Footer;
